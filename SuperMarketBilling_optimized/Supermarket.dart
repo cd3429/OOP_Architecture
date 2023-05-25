2530809d0c55456df24cd81e6cb8ae0ea90f300a
cd3429 <125622988+cd3429@users.noreply.github.com>
2023-05-25 10:01:50 +0530

    second commit

--- SuperMarketBilling_optimized/Supermarket.dart
+++ SuperMarketBilling_optimized/Supermarket.dart
@@ -1,6 +1,7 @@
 import 'Customer.dart';
 import 'Items.dart';
 import 'dart:io';
+import 'itemCatalog.dart';
 
 class Supermarket {
   List<Item> items;
@@ -26,21 +27,7 @@ class Supermarket {
   }
 
   void addSampleItems() {
-    items.add(Item(1, 'Pasta', 50));
-    items.add(Item(2, 'Lemon', 30));
-    items.add(Item(3, 'Garlic', 40));
-    items.add(Item(4, 'Pizza', 100));
-    items.add(Item(5, 'Grapes', 80));
-    items.add(Item(6, 'Watermelon', 70));
-    items.add(Item(7, 'Pineapple', 90));
-    items.add(Item(8, 'Papaya', 60));
-    items.add(Item(9, 'Kiwi ', 120));
-    items.add(Item(10, 'Strawberry', 150));
-    items.add(Item(11, 'Bread', 25));
-    items.add(Item(12, 'Milk ', 45));
-    items.add(Item(13, 'Cheese', 70));
-    items.add(Item(14, 'Chicken', 200));
-    items.add(Item(15, 'Rice ', 60));
+    itemCatalog(items);
   }
 
   void newCustomer() {
---
+++ SuperMarketBilling_optimized/itemCatalog.dart
@@ -0,0 +1,19 @@
+import 'Items.dart';
+
+itemCatalog(items) {
+  items.add(Item(1, 'Pasta', 50));
+  items.add(Item(2, 'Lemon', 30));
+  items.add(Item(3, 'Garlic', 40));
+  items.add(Item(4, 'Pizza', 100));
+  items.add(Item(5, 'Grapes', 80));
+  items.add(Item(6, 'Watermelon', 70));
+  items.add(Item(7, 'Pineapple', 90));
+  items.add(Item(8, 'Papaya', 60));
+  items.add(Item(9, 'Kiwi ', 120));
+  items.add(Item(10, 'Strawberry', 150));
+  items.add(Item(11, 'Bread', 25));
+  items.add(Item(12, 'Milk ', 45));
+  items.add(Item(13, 'Cheese', 70));
+  items.add(Item(14, 'Chicken', 200));
+  items.add(Item(15, 'Rice ', 60));
+}
